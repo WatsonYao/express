f00cea32cfa2833efb46f7e2a796193c773225ac
watson <yaoyuan131617@gmail.com>
2016-05-18 17:55:10 +0800

    update

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/reg', function (req, res) {
                 //return res.redirect('/reg');//注册失败返回主册页
                 res.send('注册失败返回主册页');
             }
-            req.session.user = newUser;//用户信息存入 session
+            //req.session.user = newUser;//用户信息存入 session
             //req.flash('success', '注册成功!');
             //res.redirect('/');//注册成功后返回主页
             res.send('注册成功后返回主页');
