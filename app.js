2e3b8f2b0dda49118454124fc03e549eee84fb68
watson <yaoyuan131617@gmail.com>
2016-05-18 16:05:29 +0800

    update

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/reg', reg);
+app.get('/test', function (req, res) {
+    res.send('hello,my love!');
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/reg.js
+++ routes/reg.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
