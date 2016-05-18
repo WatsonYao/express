0bbd00bd5d74f82bf0fe17ff595d04636f61569c
watson <yaoyuan131617@gmail.com>
2016-05-18 16:01:22 +0800

    update

--- routes/reg.js
+++ routes/reg.js
@@ -4,66 +4,7 @@ var User = require('../models/user');
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-
     res.send(req.body);
-
-    var name = req.body.name,
-        password = req.body.password,
-        password_re = req.body['password-repeat'];
-    //检验用户两次输入的密码是否一致
-
-    if (req.body.name == null) {
-        res.send('name == null');
-    }
-
-    if (req.body.password == null) {
-        res.send('password == null');
-    }
-
-    if (req.body.email == null) {
-        res.send('email == null');
-    }
-
-    if (password_re != password) {
-        res.send('两次输入的密码不一致');
-        //req.flash('error', '两次输入的密码不一致!');
-        //return res.redirect('/reg');//返回注册页
-    }
-    //生成密码的 md5 值
-    //var md5 = crypto.createHash('md5'),
-    //    password = md5.update(req.body.password).digest('hex');
-
-    var newUser = new User({
-        name: name,
-        password: password,
-        email: req.body.email
-    });
-    //检查用户名是否已经存在
-    User.get(newUser.name, function (err, user) {
-        if (err) {
-            res.send('err');
-            //req.flash('error', err);
-            //return res.redirect('/');
-        }
-        if (user) {
-            res.send('用户已存在');
-            //req.flash('error', '用户已存在!');
-            //return res.redirect('/reg');//返回注册页
-        }
-        //如果不存在则新增用户
-        newUser.save(function (err, user) {
-            if (err) {
-                res.send('注册失败返回主册页');
-                //req.flash('error', err);
-                //return res.redirect('/reg');//注册失败返回主册页
-            }
-            req.session.user = newUser;//用户信息存入 session
-
-            res.send('注册成功后返回主页');
-            //req.flash('success', '注册成功!');
-            //res.redirect('/');//注册成功后返回主页
-        });
-    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,25 +6,6 @@ var assert = require('assert');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-
-    var mongoose = require('../node_modules/mongoose');
-
-    var db = mongoose.createConnection('localhost', 'test');
-
-    var schema = mongoose.Schema({ name: 'string' });
-    var User = db.model('User', schema);
-
-    var kitty = new User({ name: 'watson' });
-    kitty.save(function (err) {
-        if (err) // ...
-            res.end('meow');
-    });
-
-
-    User.find({'name':'watson'},function(err,docs){
-        res.render('index', { title: docs});
-    });
-
     res.send('respond with a resource');
 });
 
