401bb082009645a0d924be0bbf06c6d725dc50f7
watson <yaoyuan131617@gmail.com>
2016-05-18 14:33:49 +0800

    update

--- routes/reg.js
+++ routes/reg.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var User = require('./models/user');
+var User = require('../models/user');
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
